6d68ba33bdff1a3845681f48c043090831ed37b3
paperlivelearning <info@paperlivelearning>
2024-10-30 07:53:34 +0530

    Removed code from loop

--- 2-loop_leap-year.py
+++ 2-loop_leap-year.py
@@ -1,5 +1,5 @@
 # Leap year code in Python using for loop
-
+# Added comment For Testing
 def is_leap_year(year):
     if year % 4 == 0 and (year % 100 != 0 or year % 400 == 0):
         return True
@@ -7,7 +7,6 @@ def is_leap_year(year):
         return False
 
 # Get input from the user
-start_year = int(input("Enter the starting year: "))
 end_year = int(input("Enter the ending year: "))
 
 # Check leap year for each year in the range
