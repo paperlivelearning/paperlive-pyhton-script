df81a434ba754bf8d7bf0ad8c92c42cc3dd5ac6a
paperlivelearning <info@paperlivelearning>
2024-10-30 07:56:10 +0530

    Added code in master

--- 2-loop_leap-year.py
+++ 2-loop_leap-year.py
@@ -7,8 +7,9 @@ def is_leap_year(year):
         return False
 
 # Get input from the user
-start_year = int(input("Enter the starting year: "))
-end_year = int(input("Enter the ending year: "))
+start_year = int(input("Enter the starting year: "));
+end_year = int(input("Enter the ending year: "));
+start_year = int(input("Enter the starting year: "));
 
 # Check leap year for each year in the range
 for year in range(start_year, end_year + 1):
